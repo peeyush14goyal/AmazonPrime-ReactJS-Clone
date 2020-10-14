71168bcffec68958746fe7442a42b7b672eb8f19
Peeyush Goyal <smartrohan1514@gmail.com>
2020-10-14 14:01:53 +0530

    Movies List display

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import MediaScreen from "./components/MediaScreen";
 
 function App() {
+  
   return (
     <div className="App">
       <Header />
-      <div></div>
+      <MediaScreen heading="Test" fetchURL="/discover/movie?api_key=989a8027930013244e3c2af17088dcac"/>
     </div>
   );
 }
--- src/components/MediaScreen/index.js
+++ src/components/MediaScreen/index.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./styles.css";
+import axios from "../../axios";
 
-const MediaScreen = ({img, content}) => {
+const MediaScreen = ({heading, fetchURL}) => {
+
+    const [data, setData] = useState([]);
+
+  const base_url = "https://image.tmdb.org/t/p/original/";
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get(fetchURL) 
+      setData(response.data.results);
+      return response;
+      
+    }
+    fetchData();
+  }, [fetchURL])
+
+  console.log(data);
+
     return (
         <div className="mediaScreen" >
-            <img src={img} className="mediaImg" alt=""/>
+            <div>{heading}</div>
+            <div className="banner">
+            {data.map((item, key) => {
+                return(
+                <div className="mediaDiv" key={item.id}>
+                    <img src={`${base_url}${item.backdrop_path}`} alt={item.name} className="mediaImg"/>
+                    <div>
+                <div className="title">{item.name}</div>
+                </div>
+                </div>)})}
+            
+        </div>
         </div>
     )
 }
