8d7019d6e4cdc76b8005a4b3865a8506ca38a080
Peeyush Goyal <smartrohan1514@gmail.com>
2020-10-15 16:52:36 +0530

    Fixed banner on hover

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import Header from "./components/Header";
 import MediaScreen from "./components/MediaScreen";
 
 function App() {
-  
   return (
     <div className="App">
       <Header />
-      <MediaScreen heading="Test" fetchURL="/discover/movie?api_key=989a8027930013244e3c2af17088dcac"/>
+      <MediaScreen
+        heading="Discover Movies"
+        fetchURL="/discover/movie?api_key=989a8027930013244e3c2af17088dcac"
+        API_KEY="989a8027930013244e3c2af17088dcac"
+      />
     </div>
   );
 }
--- src/components/MediaScreen/index.js
+++ src/components/MediaScreen/index.js
@@ -1,41 +1,86 @@
-import React, {useState, useEffect} from "react";
-import "./styles.css";
+import React, { useState, useEffect } from "react";
+import "./styles.scss";
 import axios from "../../axios";
+import PlayCircleFilledWhiteOutlinedIcon from "@material-ui/icons/PlayCircleFilledWhiteOutlined";
+import AddOutlinedIcon from "@material-ui/icons/AddOutlined";
 
-const MediaScreen = ({heading, fetchURL}) => {
-
-    const [data, setData] = useState([]);
-
+const MediaScreen = ({ heading, fetchURL, API_KEY }) => {
+  const [data, setData] = useState([]);
   const base_url = "https://image.tmdb.org/t/p/original/";
+  let time;
+
+  async function getMovieRuntime(movie_id) {
+    const response = await axios.get(`/movie/${movie_id}?api_key=${API_KEY}`);
+    //console.log(response.data);
+    return response;
+  }
+
+  const getRunTime = (movie_id) => {
+    getMovieRuntime(movie_id).then((response) => {
+      //console.log(response.data.runtime);
+      time = response.data.runtime;
+      console.log(time);
+      return time;
+    });
+  };
 
   useEffect(() => {
     async function fetchData() {
-      const response = await axios.get(fetchURL) 
+      const response = await axios.get(fetchURL);
       setData(response.data.results);
       return response;
-      
     }
     fetchData();
-  }, [fetchURL])
+  }, [fetchURL]);
 
-  console.log(data);
-
-    return (
-        <div className="mediaScreen" >
-            <div>{heading}</div>
-            <div className="banner">
-            {data.map((item, key) => {
-                return(
-                <div className="mediaDiv" key={item.id}>
-                    <img src={`${base_url}${item.backdrop_path}`} alt={item.name} className="mediaImg"/>
-                    <div>
-                <div className="title">{item.name}</div>
+  return (
+    <div className="mediaScreen">
+      <div className="headingBanner">{heading}</div>
+      <div className="banner">
+        {data.map((item) => {
+          const runTime = getRunTime(item.id);
+          return (
+            <div className="mediaDiv" key={item.id}>
+              <img
+                src={`${base_url}${item.backdrop_path}`}
+                alt={item.name}
+                className="mediaImg"
+              />
+              <div className="hoverData">
+                <div className="hoverHeading">
+                  <div className="playDiv">
+                    <div className="playIcon">
+                      <PlayCircleFilledWhiteOutlinedIcon
+                        className="playButton"
+                        fontSize="large"
+                      />
+                    </div>
+                    <div className="playtext">Play</div>
+                  </div>
+                  <div>
+                    <AddOutlinedIcon className="addIcon" fontSize="large" />
+                  </div>
                 </div>
-                </div>)})}
-            
-        </div>
-        </div>
-    )
-}
+                <br />
+                <div className="title">{item.title}</div>
+                <div className="overview">
+                  {item.overview.length > 90
+                    ? item.overview.substr(0, 89) + "..."
+                    : item.overview}
+                </div>
+                <div>
+                  <div>{runTime}</div>
+                  <div className="releaseYear">
+                    {item.release_date.substr(0, 4)}
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default MediaScreen;
